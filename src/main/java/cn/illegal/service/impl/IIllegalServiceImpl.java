2eacdbee1f982048fa385650efe8015ac9f5e432
lutaijun <lutaijun@chudaokeji.com>
2017-04-15 16:34:32 +0800

    解决报错

--- src/main/java/cn/illegal/service/impl/IIllegalServiceImpl.java
+++ src/main/java/cn/illegal/service/impl/IIllegalServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;

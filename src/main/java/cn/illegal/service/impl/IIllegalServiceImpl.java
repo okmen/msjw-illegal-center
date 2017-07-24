89329b701502b039d94b2a691755f19c7a323572
lutaijun <lutaijun@chudaokeji.com>
2017-07-24 17:10:58 +0800

    电子回单

--- src/main/java/cn/illegal/service/impl/IIllegalServiceImpl.java
+++ src/main/java/cn/illegal/service/impl/IIllegalServiceImpl.java
@@ -14,7 +14,6 @@ import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
